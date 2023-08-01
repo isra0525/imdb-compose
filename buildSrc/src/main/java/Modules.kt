5648a83d0bc01e6960fac2693980873e316d7685
Israel Candia <candia.israel@gmail.com>
2023-08-01 15:55:33 -0500

    Clean architecture defined

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,14 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+
+    const val loginPresentation = ":login:login_presentation"
+    const val loginDomain = ":login:login_domain"
+    const val loginData = ":login:login_data"
+
+    const val moviesPresentation = ":movies:movies_presentation"
+    const val moviesDomain = ":movies:movies_domain"
+    const val moviesData = ":movies:movies_data"
+
+}
---
+++ login/login_data/build.gradle.kts
@@ -0,0 +1,18 @@
+
+apply {
+    from("$rootDir/base-module.gradle")
+}
+
+dependencies {
+    "implementation"(project(Modules.core))
+    "implementation"(project(Modules.loginDomain))
+
+    "implementation"(Retrofit.okHttp)
+    "implementation"(Retrofit.retrofit)
+    "implementation"(Retrofit.okHttpLoggingInterceptor)
+    "implementation"(Retrofit.moshiConverter)
+    "kapt"(Room.roomCompiler)
+    "implementation"(Room.roomKtx)
+    "implementation"(Room.roomRuntime)
+    "implementation"(DataStore.dataStorePreferences)
+}
